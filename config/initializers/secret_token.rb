ac95c5eb2eb1e1025fff968323f16f7a68d5fecd
Daniel Evans <evans.daniel.n@gmail.com>
2011-07-02 21:55:11 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreamingTest::Application.config.secret_token = 'ff29485df323ba696fb34a11829a155ef601391a76e031b48cc58b4d0634bb7a6a4e5a631923e3db3111d44f787ea6383f95347c0695d23ed9de585f11db4fc6'
